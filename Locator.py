f1d9cf8d34372ff786a7cf0261d79dd092cc4477
Chong TANG <plantton@gmail.com>
2016-06-09 23:04:32 +0200

    June-09-2016, 11pm

--- Locator.py
+++ Locator.py
@@ -5,7 +5,8 @@ import os
 import numpy as np
 import time
 import fnmatch
-
+# Package imutils installed via extra command
+import imutils
 
 class Locator:
 
@@ -75,14 +76,64 @@ class Locator:
             eigenvalues = eigenvalues[0:num_components].copy()
             eigenvectors = eigenvectors[:,0:num_components].copy()
             return [eigenvalues, eigenvectors, mu]
+        
             
+        def pyramid(image, scale=1.5, minSize=(30, 30)):
+                # http://www.pyimagesearch.com/2015/03/16/image-pyramids-with-python-and-opencv/
+   	        # yield the original image
+           	yield image 
+	        # keep looping over the pyramid
+           	while True:
+          		# compute the new dimensions of the image and resize it
+          		w = int(image.shape[1] / scale)
+          		image = imutils.resize(image, width=w)
+            
+          		# if the resized image does not meet the supplied minimum
+          		# size, then stop constructing the pyramid
+          		if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
+         			break            
+          		# yield the next image in the pyramid
+          		yield image
+		            
         def sliding_window(image, stepSize, windowSize):
 	# slide a window across the image
 	      for y in xrange(0, image.shape[0], stepSize):
 		for x in xrange(0, image.shape[1], stepSize):
 			# yield the current window
 			yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])    
-
+        
+        
+        def _obj_dec_(self,image,winW,winH):
+            # loop over the image pyramid
+            X = self.createX("C:/Users/tangc/Documents/ComVi/ASM/eigenteeth2")
+            [eigenvalues, eigenvectors, mu] = self.pca(X,num_components=10)
+            for resized in self.pyramid(image, scale=1.5):
+       	    # loop over the sliding window for each layer of the pyramid
+           	for (x, y, window) in self.sliding_window(resized, stepSize=4, windowSize=(winW, winH)):
+          		# if the window does not meet our desired window size, ignore it
+          		if window.shape[0] != winH or window.shape[1] != winW:
+         			continue
+            
+          		# THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
+          		# MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
+          		# WINDOW
+          		W=np.zeros(shape=[1,winW*winH])
+          		img = cv2.imread(window)
+                        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                        gray = cv2.equalizeHist(gray)
+                        W[0,:] = gray.flatten()
+                        fi = W - mu
+                        # Projection page 7 on the paper
+                        fi_f = self.project(eigenvectors, W, mu )
+                        epsilon = np.linalg.norm(fi - fi_f)
+          		# since we do not have a classifier, we'll just draw the window
+          		clone = resized.copy()
+          		cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
+          		cv2.imshow("Window", clone)
+          		cv2.waitKey(1)
+          		time.sleep(0.025)
+  		
+  		
         if __name__ == '__main__':
             #create database of normalized images
         #    for directory in ["data/arnold", "data/obama"]:
